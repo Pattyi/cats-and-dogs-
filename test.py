78b75f6d7cb0ee9705e1a1d3d82015b1598a92b0
Pattyi <83863761+Pattyi@users.noreply.github.com>
2024-11-08 14:20:07 +0800

    Update test.py
    使用tensorflow2框架

--- test.py
+++ test.py
@@ -2,22 +2,12 @@
 """
     test.py: 用训练好的模型对随机一张图片进行猫狗预测
 """
-
-import tensorflow._api.v2.compat.v1 as tf
-tf.disable_v2_behavior()
+import tensorflow as tf
 from PIL import Image
-import input_data
-import model
 import numpy as np
-# 使用相对路径
-import os
-# 在导入部分后添加 GPU 配置
-gpu_options = tf.GPUOptions(allow_growth=True)
-config = tf.ConfigProto(gpu_options=gpu_options)
-
-import matplotlib
-matplotlib.use('TkAgg')  # 添加这行，设置后端
 import matplotlib.pyplot as plt
+import os
+
 
 def get_one_image(img_list):
     """
@@ -26,67 +16,82 @@ def get_one_image(img_list):
         返回：
             image：从图片路径列表中随机挑选的一张图片
     """
-    n = len(img_list)                  # 获取文件夹下图片的总数
-    ind = np.random.randint(0, n)      # 从 0~n 中随机选取下标
-    img_dir = img_list[ind]            # 根据下标得到一张随机图片的路径
+    n = len(img_list)  # 获取文件夹下图片的总数
+    ind = np.random.randint(0, n)  # 从 0~n 中随机选取下标
+    img_dir = img_list[ind]  # 根据下标得到一张随机图片的路径
 
-    image = Image.open(img_dir)        # 打开img_dir路径下的图片
-    image = image.resize([208, 208])   # 改变图片的大小，定为宽高都为208像素
-    image = np.array(image)            # 转成多维数组，向量的格式
+    image = Image.open(img_dir)  # 打开img_dir路径下的图片
+    image = image.resize([208, 208])  # 改变图片的大小，定为宽高都为208像素
+    image = np.array(image)  # 转成多维数组，向量的格式
+    return image
+
+
+def process_image(image):
+    """
+    处理图片函数：标准化处理
+    """
+    image = tf.cast(image, tf.float32)
+    image = tf.image.per_image_standardization(image)
+    image = tf.expand_dims(image, 0)  # 增加batch维度
     return image
 
 
 def evaluate_one_image():
+    """
+    测试函数
+    """
     # 修改成自己测试集的文件夹路径
-    # test_dir = 'data/test'
-    # test_dir = '/home/user/Dataset/cats_vs_dogs/test/'
     test_dir = os.path.join('data', 'test')
 
-    test_img = input_data.get_files(test_dir)[0]   # 获取测试集的图片路径列表
-    image_array = get_one_image(test_img)          # 从测试集中随机选取一张图片
+    # 获取测试图片的路径列表
+    from input_data import get_files
+    test_img = get_files(test_dir)[0]
 
-    # 将这个图设置为默认图，会话设置成默认对话，这样在with语句外面也能使用这个会话执行。
-    with tf.Graph().as_default():    # 参考：https://blog.csdn.net/nanhuaibeian/article/details/101862790
-        BATCH_SIZE = 1               # 这里我们要输入的是一张图(预测这张随机图)
-        N_CLASSES = 2                # 还是二分类(猫或狗)
+    # 从测试集中随机选取一张图片
+    image_array = get_one_image(test_img)
 
-        image = tf.cast(image_array, tf.float32)                    # 将列表转换成tf能够识别的格式
-        image = tf.image.per_image_standardization(image)           # 图片标准化处理
-        image = tf.reshape(image, [1, 208, 208, 3])                 # 改变图片的形状
-        logit = model.cnn_inference(image, BATCH_SIZE, N_CLASSES)   # 得到神经网络输出层的预测结果
-        logit = tf.nn.softmax(logit)                                # 进行归一化处理（使得预测概率之和为1）
+    # 创建模型
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(2)  # 2分类
+    ])
 
-        x = tf.placeholder(tf.float32, shape=[208, 208, 3])         # x变量用于占位，输入的数据要满足这里定的shape
+    # 加载训练好的模型权重
+    checkpoint_path = os.path.join('log', 'model.ckpt-9999')  # 修改为您的模型路径
+    if os.path.exists(checkpoint_path + '.index'):
+        print("从指定路径中加载模型...")
+        model.load_weights(checkpoint_path)
+        print('模型加载成功')
+    else:
+        print('模型加载失败，checkpoint文件没找到！')
+        return
 
-        # 修改成自己训练好的模型路径
-        logs_train_dir = 'log'
+    # 处理图片
+    processed_image = process_image(image_array)
 
-        saver = tf.train.Saver()
+    # 进行预测
+    prediction = model(processed_image, training=False)
+    prediction = tf.nn.softmax(prediction)
 
-        with tf.Session(config=config) as sess:
-            print("从指定路径中加载模型...")
-            ckpt = tf.train.get_checkpoint_state(logs_train_dir)   # 读取路径下的checkpoint
-            # 载入模型，不需要提供模型的名字，会通过 checkpoint 文件定位到最新保存的模型
-            if ckpt and ckpt.model_checkpoint_path:                # checkpoint存在且其存放的变量不为空
-                global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]   # 通过切割获取ckpt变量中的步长
-                saver.restore(sess, ckpt.model_checkpoint_path)    # 当前会话中，恢复该路径下模型的所有参数（即调用训练好的模型）
-                print('模型加载成功, 训练的步数为： %s' % global_step)
-            else:
-                print('模型加载失败，checkpoint文件没找到！')
+    # 获取预测结果
+    max_index = tf.argmax(prediction, axis=1)
+    score = tf.reduce_max(prediction, axis=1)
 
-            # 通过saver.restore()恢复了训练模型的参数（即：神经网络中的权重值），这样logit才能得到想要的预测结果
-            # 执行sess.run()才能运行，并返回结果数据
-            prediction = sess.run(logit, feed_dict={x: image_array})   # 输入随机抽取的那张图片数据，得到预测值
-            max_index = np.argmax(prediction)                          # 获取输出结果中最大概率的索引(下标)
-            if max_index == 0:
-                pre = prediction[:, 0][0] * 100
-                print('图片是猫的概率为： {:.2f}%'.format(pre))       # 下标为0，则为猫，并打印是猫的概率
-            else:
-                pre = prediction[:, 1][0] * 100
-                print('图片是狗的概率为： {:.2f}%'.format(pre))       # 下标为1，则为狗，并打印是狗的概率
+    if max_index[0] == 0:
+        print('图片是猫的概率为： %.2f%%' % (score[0] * 100))
+    else:
+        print('图片是狗的概率为： %.2f%%' % (score[0] * 100))
 
-    plt.imshow(image_array)                                        # 接受图片并处理
-    plt.show()                                                     # 显示图片
+    # 显示测试图片
+    plt.imshow(image_array)
+    plt.axis('off')
+    plt.show()
 
 
 if __name__ == '__main__':
