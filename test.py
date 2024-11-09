79dfa2b576cf64d64c870afce3e39cf021e3e494
Pattyi <83863761+Pattyi@users.noreply.github.com>
2024-11-09 18:12:42 +0800

    Update test.py
    优化模型调用

--- test.py
+++ test.py
@@ -12,17 +12,17 @@ import os
 def get_one_image(img_list):
     """
         输入：
-            img_list：图片路径列表
+            img_list：图片路径列表 存放的是每个图片完整的路径 data/test/6.jpg
         返回：
-            image：从图片路径列表中随机挑选的一张图片
+            image：从图片路径列表中随机挑选的一张图片，格式为NumPy格式
     """
     n = len(img_list)  # 获取文件夹下图片的总数
     ind = np.random.randint(0, n)  # 从 0~n 中随机选取下标
-    img_dir = img_list[ind]  # 根据下标得到一张随机图片的路径
+    img_dir = img_list[ind]  # 根据下标得到一张随机图片的路径 data/test/6.jpg
 
-    image = Image.open(img_dir)  # 打开img_dir路径下的图片
+    image = Image.open(img_dir)  # 打开img_dir路径下的图片 PIL格式
     image = image.resize([208, 208])  # 改变图片的大小，定为宽高都为208像素
-    image = np.array(image)  # 转成多维数组，向量的格式
+    image = np.array(image)  # 转成多维数组，向量的格式 由PIL格式->NumPy格式
     return image
 
 
@@ -45,22 +45,15 @@ def evaluate_one_image():
 
     # 获取测试图片的路径列表
     from input_data import get_files
-    test_img = get_files(test_dir)[0]
+    test_img = get_files(test_dir)[0]  # get_files()[0]得到的是图片路径的列表
 
     # 从测试集中随机选取一张图片
-    image_array = get_one_image(test_img)
+    image_array = get_one_image(test_img)  # 传入图片路径列表，得到numpy类型的一张图片
 
     # 创建模型
-    model = tf.keras.Sequential([
-        tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
-        tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
-        tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(256, activation='relu'),
-        tf.keras.layers.Dense(512, activation='relu'),
-        tf.keras.layers.Dense(2)  # 2分类
-    ])
+    # 修改后的代码（推荐）：
+    from model import CatsDogsModel
+    model = CatsDogsModel(batch_size=1, n_classes=2)  # batch_size=1因为只预测一张图片调用__init__(self, batch_size, n_classes)
 
     # 加载训练好的模型权重
     checkpoint_path = os.path.join('log', 'model.ckpt-9999')  # 修改为您的模型路径
@@ -72,11 +65,11 @@ def evaluate_one_image():
         print('模型加载失败，checkpoint文件没找到！')
         return
 
-    # 处理图片
+    # 处理单张图片
     processed_image = process_image(image_array)
 
     # 进行预测
-    prediction = model(processed_image, training=False)
+    prediction = model(processed_image, training=False)  # 调用call(self, inputs, training=None)
     prediction = tf.nn.softmax(prediction)
 
     # 获取预测结果
